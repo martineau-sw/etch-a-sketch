3aadfcd67731b53dfdef9aa229a15d2985a53adb
Seth Martineau <martineausw@gmail.com>
2024-11-04 13:22:07 -0500

    feat: add handler for grid resolution reconfiguration
    With input validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generateGrid(resolution) {
+function populateGrid(resolution) {
   const gridContainer = document.querySelector('#grid');
   for (let count = 0; count < resolution ** 2; count++) {
     const pixel = document.createElement('div');
@@ -11,4 +11,39 @@ function generateGrid(resolution) {
   }
 }
 
-generateGrid(16);
+function replaceGrid() {
+  const gridContainer = document.querySelector('#grid');
+  const newGridContainer = document.createElement('div');
+
+  document.querySelector('body').insertBefore(newGridContainer, gridContainer);
+  gridContainer.remove();
+  newGridContainer.setAttribute('id', 'grid');
+}
+
+function getUserResolution() {
+  const resolution = prompt('Enter Resolution [8, 100]');
+  const CANCELED_INPUT = '';
+  if (Number.isNaN(resolution)) {
+    if (resolution === CANCELED_INPUT) {
+      return;
+    }
+
+    alert('Resolution must be a number');
+    return;
+  }
+
+  if (!Number.isInteger(+resolution)) {
+    alert("Resolution must be an integer");
+    return;
+  }
+
+  if (+resolution < 8 || +resolution > 100) {
+    alert("Resolution must be in range [8, 100]");
+    return;
+  }
+
+  replaceGrid();
+  populateGrid(+resolution);
+}
+
+populateGrid(16);
